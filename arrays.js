4c396a7d7c38f6875614f1da10a22cedaca0b906
Max Zelinka <fourthwalljoke@gmail.com>
2017-10-24 17:02:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginningOfArray(element, array) {
+function addElementToBeginningOfArray(array, element) {
  return [...array, element]
 
 }
 
-function destructivelyAddElementToBeginningOfArray(element, array) {
+function destructivelyAddElementToBeginningOfArray(array, element) {
   array.push(element)
   return array;
 }
