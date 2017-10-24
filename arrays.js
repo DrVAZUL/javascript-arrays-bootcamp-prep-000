358540b03e6f4c9caa9a6c1eaede7650975b382a
Max Zelinka <fourthwalljoke@gmail.com>
2017-10-24 17:07:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,11 +10,4 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array;
 }
 
-function addElementToEndOfArray(array, element) {
-  return [array..., element]
-}
 
-function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element)
-  return array;
-}
