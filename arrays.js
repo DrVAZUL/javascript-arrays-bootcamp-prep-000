c93605ef579434967e0ff6a25698a3d4fad68ad9
Max Zelinka <fourthwalljoke@gmail.com>
2017-10-24 16:49:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  const array = array
-  const newArray = [element, ...array]
-  return newArray
+ return [element, ...array]
+
 }
 
 function destructivelyAddElementToBeginningOfArray(array , element) {
